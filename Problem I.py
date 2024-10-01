355ecc2888eee6ff66f1098b6dfb6440ab57dc72
MuhdRayan10 <119870649+MuhdRayan10@users.noreply.github.com>
2024-10-02 00:10:49 +0300

    Solved Problem I.py
    Interesting solution, better than recursively going through each second as that would give us TLE

--- Problem I.py
+++ Problem I.py
@@ -3,11 +3,30 @@
 from sys import stdin, stdout
 
 s, n = tuple(map(int, stdin.readline().split()))
-changes = [tuple(map(int, stdin.readline().split()) for _ in range(n)]
+
+# Storing all change moments in the format (m_i, c_i)
+changes = [tuple(map(int, stdin.readline().split())) for _ in range(n)] + [(s+1, 0)]
 
 current = (0, 0)
 speed = 0
 distance = 0
+
+
+# Sort list in terms of time of increment (m_i) 
+# Sorting in descending order to pop easily
+changes.sort(reverse=True, key=lambda x:x[0])
+
+for _ in range(n+1):
+  new_change = changes.pop()
+
+  time_diff = new_change[0] - current[0] # Constant speed during this time interval
+
+  distance += time_diff * speed
+  speed += new_change[1]
+
+  current = new_change
+  
+stdout.write(str(distance))
 
 
 
